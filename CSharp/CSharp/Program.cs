80e28a747363520fd6193a636292cfa7aa2decd5
343588387 <343588387@qq.com>
2017-07-05 13:54:32 +0800

    code

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -2,7 +2,9 @@
 using EFWCoreLib.WcfFrame.DataSerialize;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 
 namespace CSharp
@@ -11,7 +13,26 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("服务名：Books.Service");
+            Console.WriteLine("控制器：Books.Service");
+            Console.WriteLine("方法：GetBooks");
 
+            Console.WriteLine("1.使用ClientLink访问efwplus中间件服务：");
+            //使用ClientLink方式
+            CallService();
+            //Console.ReadLine();
+
+            Console.WriteLine("2.使用Http API访问efwplus中间件服务：");
+            for (int i = 0; i < 100; i++)
+                //使用API方式
+                CallAPI();
+            Console.ReadLine();
+        }
+
+
+        static void CallService()
+        {
+            
             ClientLink clientlink = new ClientLink("Books.Service");
             clientlink.CreateConnection();
 
@@ -22,9 +43,46 @@ namespace CSharp
 
             ServiceResponseData retData = clientlink.Request("bookWcfController", "GetBooks", _requestAction);
             string data = retData.GetJsonData();
+            clientlink.UnConnection();
 
             Console.WriteLine(data);
-            Console.ReadLine();
+        }
+
+        static void CallAPI()
+        {
+            Uri address = new Uri("http://36.111.200.233:7711/HttpService/");
+            string args = "{\"sysright\": {\"token\": \"\"},\"plugin\": \"Books.Service\",\"controller\": \"bookWcfController\",\"method\": \"GetBooks\"}";
+            // Create the web request
+            HttpWebRequest request = WebRequest.Create(address) as HttpWebRequest;
+
+            // Set type to POST
+            request.Method = "POST";
+            request.ContentType = "application/json; charset=utf-8";
+            // Create a byte array of the data we want to send
+            byte[] byteData = UTF8Encoding.UTF8.GetBytes(args);
+
+            // Set the content length in the request headers
+            request.ContentLength = byteData.Length;
+
+            // Write data
+            using (Stream postStream = request.GetRequestStream())
+            {
+                postStream.Write(byteData, 0, byteData.Length);
+                postStream.Flush();
+            }
+
+            string data;
+            // Get response
+            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            {
+                // Get the response stream
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+
+                // Console application output
+                data = reader.ReadToEnd();
+            }
+
+            Console.WriteLine(data);
         }
     }
 }
